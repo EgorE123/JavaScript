f74ccf2bb0a9b6691101424d9d6d2561ada0b37c
EgorE123 <dyukin.1992@bk.ru>
2025-10-16 08:49:30 +0500

    homework2

---
+++ 2/dom.js
@@ -0,0 +1,48 @@
+let box = {
+        element: document.querySelector('.box'),
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    }
+let isTop = false;
+let isBotton = false;
+let isLeft = false;
+let isRight = false;
+let interval = setInterval(() => {
+    if (box.top < window.innerHeight - 125 && isBotton == false) {
+        box.top += 10;
+        isTop = true;
+    } else if (box.left < window.innerWidth - 130 && isRight == false) {
+        box.left += 10;
+        isLeft =true;
+    } else if (box.top > 0 && isTop == true) {
+        box.top -= 10;
+        isBotton = true;
+        isLeft = true;
+    } else if (box.left > 0 && isLeft == true){
+        box.left -= 10
+        isRight = true
+    }
+    if (isRight == true && box.left == 0){
+        if (box.element.innerText == '1'){
+            box.element.innerText = '2'
+        }
+        else if (box.element.innerText == '2'){
+            box.element.innerText = '3'
+        }
+        else {
+            box.element.remove()
+            clearInterval(interval)
+        }
+        isTop = false;
+        isBotton = false;
+        isLeft = false;
+        isRight = false;
+
+    }
+    box.element.style.top = box.top + "px";
+    box.element.style.left = box.left + "px";
+    box.element.style.bottom = box.bottom + "px";
+    box.element.style.right = box.right + "px";
+}, 25);
